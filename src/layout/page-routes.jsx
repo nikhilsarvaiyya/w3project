af957f3b370fed78386a6b9f269b583538191421
nikssar <nikhilsarvaiyya@gmail.com>
2023-03-15 23:28:29 +0530

    Updates

--- src/layout/page-routes.jsx
+++
@@ -1,5 +0,0 @@
-const headerLink = [''];
-const menuLinks = ['Home', 'About', 'Contact'];
-const settingLinks = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-export { headerLink, menuLinks, settingLinks }
--- src/pages/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import ImageList from '../assets/json/ImageList.json'
-import MasonryPage from '../component/widgets/Masonry';
-
-
-export default function Contact() {
-    return (
-        <MasonryPage heading="Gallery" imageList={ImageList} />
-    );
-}
